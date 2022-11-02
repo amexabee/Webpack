c178c13f33d2203a7cdf7bc4838992f101b86b7e
amexabee <amexabee@yahoo.com>
2022-11-02 16:06:12 +0200

    Fix linters issue

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,10 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
